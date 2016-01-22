7807dc4fedc123578b14a9e4677929ce0ab153cb
t-araki <t-araki@keyportsolutions.com>
2016-01-22 18:14:21 +0900

    追加検索追加

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,36 +1,31 @@
 package hello;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .antMatchers("/", "/searchName").permitAll()
-                .antMatchers("/", "/finishSearch").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/home", "/searchName", "/finishSearch").permitAll().anyRequest()
+				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+	}
 
-    @Autowired
+	@Autowired
+	   private DataSource dataSource;
+
+	@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("1").password("1").roles("USER1");
-    }
+        auth.jdbcAuthentication().dataSource(dataSource);
+	}
 }
--- src/main/java/hello/repository/NameRepository.java
+++ src/main/java/hello/repository/NameRepository.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.test.context.ActiveProfiles;
 
 import hello.entity.Name;
 
+@ActiveProfiles("dev")
 @Repository
 public interface NameRepository extends JpaRepository<Name, Integer>{
 // find methods
 	  public List<Name> findByFirstname(String firstname);
+	  
 	  public List<Name> findByLastname(String lastname);
+	  
 	  public List<Name> findByFirstnameOrLastname(String firstname,String lastname);
 }
